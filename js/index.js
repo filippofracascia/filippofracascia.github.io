434e6353461da0cb510b5e595c8b2b0d6bacb368
filippofracascia <fracascia.filippo99@gmail.com>
2022-11-06 15:19:26 +0100

    some shapes

--- js/index.js
+++ js/index.js
@@ -9,25 +9,55 @@ renderer.setSize( canvas.offsetWidth, canvas.offsetHeight );
 renderer.setPixelRatio(canvas.devicePixelRatio);
 canvas.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const wireframe = new THREE.WireframeGeometry( geometry );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+const cubeWireframe = new THREE.WireframeGeometry( cubeGeometry );
+const cube = new THREE.Mesh( cubeGeometry );
 
-const line = new THREE.LineSegments( wireframe );
-line.material.depthTest = false;
-line.material.opacity = 0.25;
-line.material.transparent = true;
+const sphereGeometry = new THREE.SphereGeometry( 0.8, 16, 16 );
+const sphereWireframe = new THREE.WireframeGeometry( sphereGeometry );
+const sphere = new THREE.Mesh( sphereGeometry );
 
-scene.add( line );
+const coneGeometry = new THREE.ConeGeometry( 0.7, 1.4, 16 );
+const coneWireframe = new THREE.WireframeGeometry( coneGeometry );
+const cone = new THREE.Mesh( coneGeometry );
+
+const cubeLine = new THREE.LineSegments( cubeWireframe );
+cubeLine.material.depthTest = false;
+cubeLine.material.opacity = 0.3;
+cubeLine.material.transparent = true;
+
+const sphereLine = new THREE.LineSegments( sphereWireframe );
+sphereLine.material.depthTest = false;
+sphereLine.material.opacity = 0.2;
+sphereLine.material.transparent = true;
+
+const coneLine = new THREE.LineSegments( coneWireframe );
+coneLine.material.depthTest = false;
+coneLine.material.opacity = 0.2;
+coneLine.material.transparent = true;
+
+scene.add( cubeLine );
+scene.add( sphereLine );
+scene.add( coneLine );
 
 camera.position.z = 5;
+
+cubeLine.position.x = -1;
+sphereLine.position.x = 1;
+sphereLine.position.y = 2;
+coneLine.position.x = 2;
+coneLine.position.y = -1;
 
 function animate() {
     requestAnimationFrame( animate );
 
-    line.rotation.x += 0.01;
-    line.rotation.y += 0.01;
+    cubeLine.rotation.x += 0.01;
+    cubeLine.rotation.y += 0.01;
+
+    sphereLine.rotation.x -= 0.001;
+    sphereLine.rotation.y -= 0.01;
+
+    coneLine.rotation.y += 0.01;
 
     renderer.render( scene, camera );
 };
